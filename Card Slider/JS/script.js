e869b9fc842f506f2c7dc5e6f020465fd999b1e7
krishna0712 <krishnaacharya072007@gmail.com>
2024-09-20 23:26:50 +0530

    finishh

--- Card Slider/JS/script.js
+++ Card Slider/JS/script.js
@@ -1,13 +1,32 @@
 let swiperCards = new Swiper('.card__content', {
+  slidesPerView: 3,
   loop: true,
-  spaceBetween:30,
-  grabCursor:true,
+  spaceBetween: 30,
+  grabCursor: true,
   pagination: {
     el: '.swiper-pagination',
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+// Function to update slidesPerView based on screen width
+const updateSlidesPerView = () => {
+  const width = window.innerWidth;
+  if (width < 480) {
+    swiperCards.params.slidesPerView = 1;
+  } else if (width < 768) {
+    swiperCards.params.slidesPerView = 2;
+  } else {
+    swiperCards.params.slidesPerView = 3;
+  }
+  swiperCards.update();
+};
+
+// Initial check
+updateSlidesPerView();
+
+// Add event listener for screen resize
+window.addEventListener('resize', updateSlidesPerView);
